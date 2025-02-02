ae51752d050c3f1d215b5ca76288fa67ec8f23d7
Derek <gameggsqb@gmail.com>
2025-02-02 22:45:16 +0100

    Create 1.php

---
+++ 1.php
@@ -0,0 +1,6 @@
+$link = new mysqli('localhost','NAME','PASSWORD','kano');
+mysqli_set_charset($link, 'utf8');
+if ($link->connect_error) {
+    die("Failed to connect: " . $conn->connect_error);
+}
+$ranking = "kano_rank";
